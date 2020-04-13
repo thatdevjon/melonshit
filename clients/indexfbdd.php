7b57d290ecfe0eed3f7f6bb573ec30c738d9d0c0
thatdevjon <56226116+thatdevjon@users.noreply.github.com>
2020-04-13 21:28:26 +0100

    New Melonshit Website

---
+++ clients/indexfbdd.php
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="utf-8"?>
+            <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+                <channel>
+                    <atom:link href="https://www.meloncube.net/clients/index.php?rp=/announcements/rss" rel="self" type="application/rss+xml" />
+                    <title><![CDATA[MelonCube]]></title>
+                    <description><![CDATA[MelonCube Сповіщення RSS]]></description>
+                    <link>https://www.meloncube.net/clients/index.php?rp=/announcements</link>
+                    
+                </channel>
+            </rss>
